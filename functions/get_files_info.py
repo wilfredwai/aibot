f1641e576bbd37ac87e469b8dcd812805aada0fd
wilfredwai <wilfredwilfred@gmail.com>
2026-02-16 15:36:02 +0800

    add test_get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,19 +6,18 @@ def get_files_info(working_directory, directory="."):
         print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
 
     if not os.path.isdir(directory):
-        print("f'Error: "{directory}" is not a directory'")
+        print(f'Error: "{directory}" is not a directory')
     
-    try:
-        path = target_dir
-        file_list = os.listdir(path)
-        for file in file_list:
-            
-#- README.md: file_size=1032 bytes, is_dir=False
-#- src: file_size=128 bytes, is_dir=True
-#- package.json: file_size=1234 bytes, is_dir=False
-
-
-    except FileNotFoundError:
-        print(f"Error: The directory '{path}' was not found.")
-    except PermissionError:
-        print(f"Error: Insufficient permissions to access '{path}'.")
+    
+    path = target_dir
+    file_list = os.listdir(path)
+    for file in file_list:
+        file_path = os.path.join(path, file)
+        try:
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            print(f'  - {file}: file_size={file_size} bytes, is_dir={is_dir}')
+        except FileNotFoundError:
+            print(f'Error: File "{file}" was not found.')
+        except PermissionError:
+            print(f'Error: Insufficient permissions to access "{file}".')
---
+++ functions/test_get_files_info.py
@@ -0,0 +1,1 @@
+get_files_info("calculator", ".")
